82732177b73424b71588e5d61c4962fd491547dc
tssvett <tssvett@mail.ru>
2024-02-24 13:11:53 +0300

    initial commit

---
+++ rest-service/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,12 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting")
+    public Greeting greeting() {
+        return new Greeting(1, "Hello, World!");
+    }
+}

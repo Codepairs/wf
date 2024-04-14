a8d9fcc0d5e7660fc71ce955391e354947f24434
Sergey <goosewww010@gmail.com>
2024-04-15 00:10:33 +0400

    added user controller for website

--- backend/src/main/java/org/example/controller/RegistrationController.java
+++ backend/src/main/java/org/example/controller/RegistrationController.java
@@ -1,12 +1,14 @@
 package org.example.controller;
 
 import com.example.myapp.dto.full.UserFullDto;
+import com.example.myapp.dto.update.UserUpdateDto;
 import com.example.myapp.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class RegistrationController {
@@ -14,6 +16,14 @@ public class RegistrationController {
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("password", new String());
+        return "registration";
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserUpdateDto userDto = new UserUpdateDto();
+        model.addAttribute("user", userDto);
         return "registration";
     }
 
@@ -22,12 +32,13 @@ public class RegistrationController {
                                @RequestParam(required = true) String password,
                                @RequestParam(required = true) String name,
                                Model model) {
-        UserFullDto user = new UserFullDto();
-        user.name = name;
-        user.email = email;
-        user.password = password;
+        UserUpdateDto user = new UserUpdateDto();
+        user.builder().email(email).password(password).name(name).build();
+
+        // TODO validate user
         // TODO save user to database
         // userDao.save(user);
+
 
         // TODO send out registration email
         // mailService.sendRegistrationEmail(user);
